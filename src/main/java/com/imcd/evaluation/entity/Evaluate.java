9bae16e7ee5ef7eeee308b552ca62a6065eed77f
sooyun <55366664+imcodding@users.noreply.github.com>
2022-11-21 15:50:32 +0900

    불필요한 파일 제거

--- src/main/java/com/imcd/evaluation/entity/Evaluate.java
+++
@@ -1,7 +0,0 @@
-package com.imcd.evaluation.entity;
-
-import javax.persistence.Entity;
-
-@Entity
-public class Evaluate {
-}
--- src/main/java/com/imcd/evaluation/repository/EvaluateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.imcd.evaluation.repository;
-
-import com.imcd.evaluation.entity.Evaluate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EvaluateRepository extends JpaRepository<Evaluate, Long> {
-}
